f1ba013854d3ca2de7e7ab955612808e8809c59c
sweet <MNealcos@gmail.com>
2021-03-14 02:24:39 +0200

    3 Screens Remaining

---
+++ app/src/main/java/com/panselutele/demoney/IncomeAdd.java
@@ -0,0 +1,14 @@
+package com.panselutele.demoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IncomeAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income_add);
+    }
+}
